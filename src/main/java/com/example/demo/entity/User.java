9aa72f4493b19cc6cd8817a59cc35cb37f195c3e
yangkangou <459616756@qq.com>
2020-05-31 13:20:04 +0800

    add redis
    Signed-off-by: yangkangou <459616756@qq.com>

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+
 public class User {
     private Integer id;
     private String userName;
---
+++ src/main/java/com/example/demo/mapper/Normalized.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+
+@Mapper
+@Repository
+public interface Normalized {
+
+
+
+
+
+
+}
